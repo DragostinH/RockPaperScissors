ecd6a02dd0be2dd4c90b5a167940e1327d334849
DragostinH <dragostin.hristov@gmail.com>
2021-12-21 21:14:34 +0200

    initial commit to new branch

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-let test = 0;
 
 
 
@@ -21,7 +20,6 @@ function getComputerHand() {
     return hand;
 }
 
-
 function getUserHand() {
     let input = prompt("Please choose your hand.");
 
@@ -34,8 +32,6 @@ function getUserHand() {
     console.log(`User has chosen ${input}`);
     return input.toLowerCase();
 }
-
-
 
 function rockPaperScissorsGame() {
     let n = parseInt(prompt("Please enter the amount of rounds you would like to play"));
@@ -103,3 +99,16 @@ function rockPaperScissorsGame() {
 }
 
 console.log(rockPaperScissorsGame());
+
+/*
+
+In our UI, the player should be able to play the game by clicking on buttons rather than typing their answer in a prompt.
+
+    For now, remove the logic that plays exactly five rounds.
+    Create three buttons, one for each selection. Add an event listener to the buttons that calls your playRound function with the correct playerSelection every time a button is clicked. (you can keep the console.logs for this step)
+    Add a div for displaying results and change all of your console.logs into DOM methods.
+    Display the running score, and announce a winner of the game once one player reaches 5 points.
+    You will likely have to refactor (rework/rewrite) your original code to make it work for this. That’s OK! Reworking old code is an important part of a programmer’s life.
+    Once you’re all done with your UI and make sure everything’s satisfactory, it’s time to merge our current branch rps-ui to our main branch.
+
+    */
